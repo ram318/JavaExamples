41bf4211e5324e5ae919b0d44e6d4a7d695f733d
ram <ramarao.a.v@gmail.com>
2021-02-12 21:42:29 +0530

    Included IntStream samples

---
+++ src/main/java/j8streamexamples/IntStreamSamples.java
@@ -0,0 +1,22 @@
+package j8streamexamples;
+
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class IntStreamSamples {
+    public static void main(String[] args) {
+        IntStream.range(1,11).reduce((a,b)->a+b).ifPresent(s-> System.out.println(s));
+        IntStream.rangeClosed(1,11).reduce((a,b)->a+b).ifPresent(s-> System.out.println(s));
+
+        int sum = IntStream.range(1,11).reduce(0,(a,b)->a+b);
+        System.out.println(sum);
+        int sum1 = IntStream.range(1,11).parallel().reduce(0,(a,b)->a+b);
+        System.out.println(sum1);
+        System.out.println(IntStream.range(1,11).parallel().reduce(0,Integer::sum));
+        IntStream.range(1,11).forEach(System.out::println);
+
+        Function<Integer, Integer> myFunction = i -> i * i;
+        IntStream.range(1,11).forEach(a->System.out.format("%s ",myFunction.apply(a)));
+
+    }
+}
